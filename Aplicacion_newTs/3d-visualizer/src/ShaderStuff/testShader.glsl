7342a4b5b95d903ebc77d80cdf01c54071fee2ac
Daniel2000815 <danizufrique@gmail.com>
2023-02-20 23:45:36 +0100

    Bases reducidas y documentación

--- Aplicacion_newTs/3d-visualizer/src/ShaderStuff/testShader.glsl
+++ Aplicacion_newTs/3d-visualizer/src/ShaderStuff/testShader.glsl
@@ -2,27 +2,6 @@
 precision mediump float;
 #endif
 
-uniform vec2 u_resolution;
-uniform vec2 u_mouse;
-uniform mat4 viewMatrix;
-
-uniform mat4 viewMatrix;
-
-// Camera position
-uniform vec3 cameraPosition;
-
-// Point that the camera is looking at
-uniform vec3 targetPosition;
-
-// Up direction for the camera
-uniform vec3 upDirection;
-
-// Sensitivity of the camera movement (adjust as needed)
-const float sensitivity=.01;
-
-// Previous mouse position (used to calculate mouse delta)
-vec2 prevMousePos;
-
 // Constants
 const int MAX_MARCHING_STEPS=255;
 const float MIN_DIST=0.;
@@ -30,6 +9,15 @@ const float MAX_DIST=100.;
 const float PRECISION=.0001;
 const float EPSILON=.0005;
 const float PI=3.14159265359;
+
+const vec3 u_specular=vec3(1.,0.,1.);
+const vec3 u_diffuse=vec3(1.,0.,0.);
+const vec3 u_ambient=vec3(.2);
+const float u_smoothness=10.;
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+const float u_zoom=1.;
 
 struct Material
 {
@@ -92,7 +80,110 @@ mat3 identity(){
   );
 }
 
-// === SDFs ===
+vec3 sdfTwist(in vec3 p,in float k)
+{
+  float c=cos(k*p.y);
+  float s=sin(k*p.y);
+  mat2 m=mat2(c,-s,s,c);
+  vec3 q=vec3(m*p.xz,p.y);
+  return q;
+}
+
+vec3 sdfBend(in vec3 p,in float k)
+{
+  float c=cos(k*p.x);
+  float s=sin(k*p.x);
+  mat2 m=mat2(c,-s,s,c);
+  vec3 q=vec3(m*p.xy,p.z);
+  return q;
+}
+vec3 sdfRepeat(in vec3 p,in float s,in vec3 lim)
+{
+  return p-s*clamp(floor(p/s+.5),-lim,lim);
+}
+
+float sdfUnion(float d1,float d2){
+  
+  return min(d1,d2);
+}
+
+float sdfSmoothUnion(float d1,float d2,float k){
+  float h=clamp(.5+.5*(d2-d1)/k,0.,1.);
+  return mix(d2,d1,h)-k*h*(1.-h);
+}
+
+float sdfDifference(float d1,float d2){
+  
+  return max(d1,-d2);
+}
+
+float sdfSmoothDifference(float d1,float d2,float k){
+  float h=clamp(.5-.5*(d2+d1)/k,0.,1.);
+  return mix(d1,-d2,h)+k*h*(1.-h);
+}
+
+float sdfIntersection(float d1,float d2){
+  
+  return max(d1,d2);
+}
+
+float sdfSmoothIntersection(float d1,float d2,float k){
+  float h=clamp(.5-.5*(d2-d1)/k,0.,1.);
+  return mix(d2,d1,h)+k*h*(1.-h);
+}
+
+// https://stackoverflow.com/questions/34050929/3d-point-rotation-algorithm
+vec3 sdfRotate(vec3 p,vec3 ang){
+  float pitch=ang.x;
+  float roll=ang.y;
+  float yaw=ang.z;
+  
+  float cosa=cos(yaw);
+  float sina=sin(yaw);
+  
+  float cosb=cos(pitch);
+  float sinb=sin(pitch);
+  
+  float cosc=cos(roll);
+  float sinc=sin(roll);
+  
+  float Axx=cosa*cosb;
+  float Axy=cosa*sinb*sinc-sina*cosc;
+  float Axz=cosa*sinb*cosc+sina*sinc;
+  
+  float Ayx=sina*cosb;
+  float Ayy=sina*sinb*sinc+cosa*cosc;
+  float Ayz=sina*sinb*cosc-cosa*sinc;
+  
+  float Azx=-sinb;
+  float Azy=cosb*sinc;
+  float Azz=cosb*cosc;
+  
+  float px=p.x;
+  float py=p.y;
+  float pz=p.z;
+  
+  p.x=Axx*px+Axy*py+Axz*pz;
+  p.y=Ayx*px+Ayy*py+Ayz*pz;
+  p.z=Azx*px+Azy*py+Azz*pz;
+  
+  return p;
+}
+
+vec3 sdfTranslate(vec3 p,vec3 t){
+  return p-t;
+}
+
+vec3 sdfScale(vec3 p,vec3 s)
+{
+  return(p/s)*s;
+}
+
+Surface minWithColor(Surface obj1,Surface obj2){
+  if(obj2.sd<obj1.sd)return obj2;
+  return obj1;
+}
+
 float sphere(vec3 p,float radius)
 {
   return length(p)-radius;
@@ -120,30 +211,20 @@ float line(in vec3 p,in vec3 start,in vec3 end,in float thickness){
   vec3 ba=end-start;
   vec3 pa=p-start;
   float h=clamp(dot(pa,ba)/dot(ba,ba),0.,1.);
-  
   return length(pa-h*ba)-thickness;
 }
 
-Surface minWithColor(Surface obj1,Surface obj2){
-  if(obj2.sd<obj1.sd)return obj2;
-  return obj1;
-}
-
-float f(vec3 p)
-{
-  float x=p.r;
-  float y=p.g;
-  float z=p.b;
-  
-  return(x*x+y*y+z*z-12.)/sqrt(4.*x*x+4.*y*y+4.*z*z);
-}
-
 Surface map(vec3 p){
-  Material mat=Material(vec3(1.),vec3(1.,0.,0.),vec3(.2),10.);
+  Material mat=Material(u_specular,u_diffuse,u_ambient,u_smoothness);
+  float sphere=sphere(p-vec3(.5),1.);
+  float box=box(p+vec3(.5),vec3(1.));
   
-  float sphere=f(p);
-  
-  Surface co=Surface(sphere,mat);
+  Surface co=Surface(sdfSmoothUnion(sphere,box,.5),mat);
+  co=Surface(sdfUnion(box,sphere),mat);
+  co=Surface(sdfSmoothIntersection(sphere,box,.2),mat);
+  co=Surface(sdfIntersection(box,sphere),mat);
+  co=Surface(sdfSmoothDifference(box,sphere,.1),mat);
+  // co=Surface(sdfDifference(box,sphere),mat);
   
   return co;
 }
@@ -221,44 +302,13 @@ vec3 calcNormal(in vec3 p){
     ));
   }
   
-  mat4 lookAt(vec3 cameraPosition,vec3 targetPosition,vec3 upDirection){
-    vec3 zAxis=normalize(targetPosition-cameraPosition);
-    vec3 xAxis=normalize(cross(upDirection,zAxis));
-    vec3 yAxis=cross(zAxis,xAxis);
-    
-    mat4 viewMatrix=mat4(
-      vec4(xAxis,0.),
-      vec4(yAxis,0.),
-      vec4(zAxis,0.),
-      vec4(0.,0.,0.,1.)
-    );
-    
-    viewMatrix[3]=vec4(-dot(xAxis,cameraPosition),-dot(yAxis,cameraPosition),-dot(zAxis,cameraPosition),1.);
-    
-    return viewMatrix;
-  }
-  
   void main()
   {
-    // nuevo
-    // Calculate the mouse delta since the last frame
-    vec2 mouseDelta=vec2(u_mouse.xy)-prevMousePos;
-    
-    // Update the previous mouse position
-    prevMousePos=vec2(u_mouse.xy);
-    
-    // Adjust the camera position based on the mouse delta
-    cameraPosition=cameraPosition+mouseDelta*sensitivity;
-    
-    // Calculate the view matrix based on the camera position, target position, and up direction
-    viewMatrix=lookAt(cameraPosition,targetPosition,upDirection);
-    
-    // Transform the ray direction using the inverse of the view matrix
-    vec3 rayDirection=(viewMatrix*vec4(rayDirection,0.)).xyz;
-    // ===
-    
     vec2 uv=(gl_FragCoord.xy-.5*u_resolution.xy)/u_resolution.y;
-    vec2 mouseUV=u_mouse.xy/u_resolution.xy;// [0,1]
+    vec2 mouseUV=vec2(.5);
+    
+    if(u_mouse.x>0.||u_mouse.y>0.)
+    mouseUV=u_mouse.xy/u_resolution.xy;// [0,1]
     
     vec3 backgroundColor=vec3(.835,1.,1.);
     vec3 col=vec3(0.);
@@ -266,7 +316,8 @@ vec3 calcNormal(in vec3 p){
     vec3 lookAt=vec3(0.);
     vec3 eye=vec3(0,5,0);
     
-    float cameraRadius=2.;
+    float cameraRadius=u_zoom;
+    
     eye.yz=eye.yz*cameraRadius*rotate2d(mix(PI,0.,mouseUV.y));
     eye.xz=eye.xz*rotate2d(mix(-PI,PI,mouseUV.x))
     +vec2(lookAt.x,lookAt.z);
